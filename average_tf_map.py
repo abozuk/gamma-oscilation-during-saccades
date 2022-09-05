0c407cf2da3c8bd642364823c59f6a15ea8c0f20
ADKulesza <ad.kulesza122@gmail.com>
2022-09-05 21:55:39 +0200

    Average tf map

--- average_tf_map.py
+++ average_tf_map.py
@@ -2,20 +2,31 @@ import numpy as np
 import pylab as py
 
 
-def average_tf(filename):
+def average_tf(filename, x_shape=10, y_shape=100):
     data = np.load(filename)
-    average_data = np.zeros((19, 2, 60, 500))
-    for i in range(0, data.shape[2]):
-        for j in range(0, data.shape[0]):
-            average_data[:, i, :, :] += data[j, :, i, :, :]
-        print(average_data)
-        average_data = average_data / data.shape[0]
+    data = data[:4]
+
+    # TODO do uogólnienia, jeśli wymiary by się nie dzieliły całkowicie
+    f_shape = data.shape[3]//x_shape
+    t_shape = data.shape[4]//y_shape
+    average_data = np.zeros((data.shape[0], 19, 2,
+                             f_shape,
+                             t_shape))
+
+    for x in range(0, f_shape):
+        for y in range(0, t_shape):
+            average_data[:, :, :, x, y] = np.mean(data[:,:,:,x:x+x_shape,y:y+y_shape], axis=(3,4))
+
     return average_data
 
+if __name__ == "__main__":
+    filename = 'output/data_matrix.npy'
+    average = average_tf(filename)
+    print(average.shape)
+    avg1 = np.mean(average[:,:,0,:,:], axis=2)
+    avg2 = np.mean(average[:, :, 1, :, :], axis=2)
+    print(avg1 - avg2)
 
-filename = 'data_matrix.npy'
-average = average_tf(filename)
-
-#żeby sprawdzić czy mapki w miarę z sensem
-py.imshow(average[0, 0, :, :], aspect='auto', origin='lower', interpolation='nearest')
-py.show()
+    #żeby sprawdzić czy mapki w miarę z sensem
+    # py.imshow(average[0, 0, :, :], aspect='auto', origin='lower', interpolation='nearest')
+    # py.show()
